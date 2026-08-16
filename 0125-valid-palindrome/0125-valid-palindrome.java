34c1458f2f42a4c405d7b46b91e93d9a5e94ec8f
Kale Mukteswara Leela Datta <155612937+Datta1112@users.noreply.github.com>
2026-08-16 23:28:19 +0530

    Time: 14 ms (28.66%), Space: 46.3 MB (29.25%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String result=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        int n=result.length();
+        for(int i=0;i<n/2;i++){
+            if(result.charAt(i)!=result.charAt(n-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
