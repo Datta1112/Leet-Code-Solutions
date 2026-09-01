c72316a7b7e24d449ae0964e437050f937d21e96
Kale Mukteswara Leela Datta <155612937+Datta1112@users.noreply.github.com>
2026-09-01 15:14:19 +0530

    Time: 10 ms (81.75%), Space: 49.6 MB (97.87%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isShipped(int[] weights,int days,int max){
+        int temp=0;
+        int n=weights.length;
+        int usedays=1;
+        for(int i=0;i<n;i++){
+            if(weights[i]>max){
+                return false;
+            }
+            if(temp + weights[i]>max){
+                usedays++;
+                temp=weights[i];
+            }else{
+                temp+=weights[i];
+            }
+        }
+        if(usedays<=days){
+                return true;
+            }
+        return false;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        int l=1;
+        int r=100000000;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(isShipped(weights,days,mid)){
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return l;
+    }
+}
