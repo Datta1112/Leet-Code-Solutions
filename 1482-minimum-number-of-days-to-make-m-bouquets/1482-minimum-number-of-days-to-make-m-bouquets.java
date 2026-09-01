3b08afa6c4edd4507a9444bbac0a0420fda65f8f
Kale Mukteswara Leela Datta <155612937+Datta1112@users.noreply.github.com>
2026-09-01 14:21:55 +0530

    Time: 24 ms (9.48%), Space: 82.3 MB (92.15%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isBloom(int blooms[],int m,int k,int day){
+        int took=0;
+        int n=blooms.length;
+        for(int i=0;i<n;i++){
+            if(blooms[i]<=day){
+                took++;
+            }else{
+                took=0;
+            }
+            if(took==k){
+                m-=1;
+                took=0;
+            }
+            if(m<=0){
+                return true;
+            }
+        }
+        return false;
+    }
+    public int minDays(int[] bloomDay, int m, int k) {
+        int l=1;
+        int r=1000000000;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(isBloom(bloomDay,m,k,mid)){
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        if((long)m*k>bloomDay.length){
+            return -1;
+        }
+        return l;
+    }
+}
