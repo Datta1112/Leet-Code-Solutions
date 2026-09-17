fbd68f526e17636c5209b2dfad24be784f822eb4
Kale Mukteswara Leela Datta <155612937+Datta1112@users.noreply.github.com>
2026-09-17 14:46:56 +0530

    Time: 63 ms (7.27%), Space: 79.2 MB (25.7%) - LeetHub

---
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPossible(int[] position,int m,int k){
+        Arrays.sort(position);
+        int last=position[0];
+        m-=1;
+        for(int i=1;i<position.length;i++){
+            int temp=position[i]-last;
+            if(temp>=k){
+                last=position[i];
+                m-=1;
+            }
+        }
+        if(m>0){
+            return false;
+        }
+        return true;
+    }
+    public int maxDistance(int[] position, int m) {
+        int l=1;
+        int r=1000000000;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(isPossible(position,m,mid)){
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return r;
+    }
+}
