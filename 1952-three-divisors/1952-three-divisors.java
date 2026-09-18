0ce03226af1a719b72f8523cb6e48b5f2e9ef5c5
Kale Mukteswara Leela Datta <155612937+Datta1112@users.noreply.github.com>
2026-09-18 11:22:23 +0530

    Time: 1 ms (76.86%), Space: 42 MB (87.71%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.java
+++ 1952-three-divisors/1952-three-divisors.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isThree(int n) {
-        int count=0;
-        for(int i=1;i<=n;i++){
+        int count=1;
+        for(int i=2;i<=n;i++){
             if(n%i==0){
                 count++;
             }
