f37b5537bcdfb402fe3f726e0d5251e5cfd2acba
Kale Mukteswara Leela Datta <155612937+Datta1112@users.noreply.github.com>
2026-09-18 22:41:57 +0530

    Time: 78 ms (10.89%), Space: 70 MB (38.49%) - LeetHub

---
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPossible(int[] nums,int maxOperations,int k){
+        for(int i=0;i<nums.length;i++){
+            int a=nums[i];
+            if(a>k){
+                int ans=(int) Math.ceil((double) a/k);
+                maxOperations-=ans-1; 
+            }   
+            if(maxOperations<0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public int minimumSize(int[] nums, int maxOperations) {
+        int l=1;
+        int r=1000000000;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(isPossible(nums,maxOperations,mid)){
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return l;
+    }
+}
